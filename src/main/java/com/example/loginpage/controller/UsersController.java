6fc92babff47f84586611f0e088410ba3e3997e6
Nagy László Levente <nagylacika2002@gmail.com>
2023-10-12 20:56:45 +0200

    Login works personal page doesn't

--- src/main/java/com/example/loginpage/controller/UsersController.java
+++ src/main/java/com/example/loginpage/controller/UsersController.java
@@ -5,6 +5,8 @@ import com.example.loginpage.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -17,32 +19,48 @@ public class UsersController {
     @Autowired
     private UsersService usersService;
 
+    private final WebClient webClient;
+
+    @Autowired
+    public UsersController(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("http://localhost:8080").build();
+    }
+
+    public Mono<UsersModel> getData(String name) {
+        return webClient.get()
+                .uri("/{name}", name)
+                .retrieve()
+                .bodyToMono(UsersModel.class);
+    }
+
 
     @GetMapping("/register")
     public String getRegisterPage() {
         return "register_page";
     }
 
-    @GetMapping("/login")
+    @GetMapping("-")
     public String getLoginPage() {
         return "login_page";
     }
 
     @PostMapping("/register")
-    public String register(@RequestBody UsersModel usersModel){
+    public String register(@RequestBody UsersModel usersModel) {
         System.out.println("Login: " + usersModel);
-        var user = usersService.registerUser(usersModel.getLogin(),usersModel.getPassword(),usersModel.getEmail());
-        return user == null ? "error_page": "login_page";
+        var user = usersService.registerUser(usersModel.getLogin(), usersModel.getPassword(), usersModel.getEmail());
+        return user == null ? "Error" : user.getLogin();
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody UsersModel usersModel){
+    public String login(@RequestBody UsersModel usersModel) {
         System.out.println("Login: " + usersModel);
-        var userAuthedticated = usersService.authenticate(usersModel.getLogin(),usersModel.getPassword());
-        return userAuthedticated == null ? "error_page": "personal_page";
+        var userAuthenticated = usersService.authenticate(usersModel.getLogin(), usersModel.getPassword());
+        System.out.println(userAuthenticated);
+        return userAuthenticated == null ? "Error" : userAuthenticated.getLogin();
     }
+
     @GetMapping("/users")
-    public List<UsersModel> allUsers(){
+    public List<UsersModel> allUsers() {
         return usersService.getAllUsers();
     }
 }
