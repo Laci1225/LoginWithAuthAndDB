5c3cb64ddeb1c0a17152de93210e8d63a1cc2316
Nagy László Levente <nagylacika2002@gmail.com>
2023-11-16 17:45:46 +0100

    Password encoded

--- src/main/java/com/example/loginpage/service/UsersService.java
+++ src/main/java/com/example/loginpage/service/UsersService.java
@@ -2,34 +2,44 @@ package com.example.loginpage.service;
 
 import com.example.loginpage.model.UsersModel;
 import com.example.loginpage.repository.UsersRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class UsersService {
 
-    @Autowired
     private UsersRepository usersRepository;
 
-    public UsersModel registerUser(String login, String password, String email){
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public UsersModel registerUser(String login, String password, String email) {
         var users = usersRepository.findAll();
-        if (users.stream().map(UsersModel::getLogin).toList().contains(login))
-        {
+        if (users.stream().map(UsersModel::getLogin).toList().contains(login)) {
             return null;
         }
-        if (login != null && password != null){
+        if (login != null && password != null) {
             var usersModel = new UsersModel();
             usersModel.setLogin(login);
-            usersModel.setPassword(password);
+
+            String hashedPassword = passwordEncoder.encode(password);
+            usersModel.setPassword(hashedPassword);
+
             usersModel.setEmail(email);
             return usersRepository.save(usersModel);
         }
         return null;
     }
-    public UsersModel authenticate(String login, String password){
-        return usersRepository.findByLoginAndPassword(login,password).orElse(null);
+
+    public UsersModel authenticate(String login, String password) {
+        UsersModel user = usersRepository.findByLogin(login).orElse(null);
+        if (user != null && passwordEncoder.matches(password, user.getPassword())) {
+            return user;
+        }
+        return null;
     }
 
     public List<UsersModel> getAllUsers() {
